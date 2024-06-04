807d3dcc2b22905d0d5e78302a53399bce6d6e3b
thanks299 <thanksagbeble@gmail.com>
2024-06-04 14:32:56 -0700

    api advance

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+A Script that queries subscribers on a given Reddit subreddit.
+"""
+
+import requests
+
+def number_of_subscribers(subreddit):
+    url = "https://www.reddit.com/r/programming/about.json"
+    headers = {"User-Agent": "Your custom user agent here"}
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        data = response.json()
+        subscribers  = data['data']['subscribers']
+        return data["data"]["subscribers"]
+    else:
+        return 0
---
+++ 0x16-api_advanced/README.md
@@ -0,0 +1,1 @@
+this is my 0x16-api_advanced readme file
