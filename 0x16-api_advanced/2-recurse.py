6dc7811ae990d9f08d83d4d1ded461b7d4a27e7f
thanks299 <thanksagbeble@gmail.com>
2024-06-04 16:04:54 -0700

    api advances

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,32 +1,39 @@
 #!/usr/bin/python3
-"""Contains recurse function"""
+"""Recursive function that queries the Reddit API"""
+
 
 import requests
 
 
 def recurse(subreddit, hot_list=[], after="", count=0):
-    """Returns a list of titles of all hot posts on a given subreddit."""
-    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
-    headers = {
-        "User-Agent": "0x16-api_advanced:project:\
-v1.0.0 (by /u/firdaus_cartoon_jr)"
-    }
+    """Returns a list of titles of all hot posts on a subreddit"""
+    link = 'https://www.reddit.com/r/{}/hot/.json'
+    usr = {'User-Agent': 'Mozilla/5.0'}
     params = {
         "after": after,
         "count": count,
         "limit": 100
     }
-    response = requests.get(url, headers=headers, params=params,
-                            allow_redirects=False)
-    if response.status_code == 404:
+
+    try:
+        response = requests.get(link.format(subreddit),
+                                headers=usr,
+                                params=params,
+                                allow_redirects=False)
+        if response.status_code == 404:
+            return None
+
+        results = response.json().get("data")
+        after = results.get("after")
+        count += results.get("dist")
+
+        for c in results.get("children"):
+            hot_list.append(c.get("data").get("title"))
+
+        if after is not None:
+            return recurse(subreddit, hot_list, after, count)
+
+        return hot_list
+
+    except Exception:
         return None
-
-    results = response.json().get("data")
-    after = results.get("after")
-    count += results.get("dist")
-    for c in results.get("children"):
-        hot_list.append(c.get("data").get("title"))
-
-    if after is not None:
-        return recurse(subreddit, hot_list, after, count)
-    return hot_list
