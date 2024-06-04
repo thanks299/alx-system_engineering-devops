d873b72fb4f30f33dcf8d61904fdd13831d56d63
thanks299 <thanksagbeble@gmail.com>
2024-06-04 14:49:59 -0700

    api adavances

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,11 +7,12 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-    url = "https://www.reddit.com/r/programming/about.json"
+    url = "https://oauth.reddit.com/r/programming/about.json"
     headers = {"User-Agent": "Mozilla/5.0"}
-    response = requests.get(url, headers=headers)
+    response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 200:
         data = response.json()
-        return data["data"]["subscribers"]
+        subscribers = data["data"]["subscribers"]
+        return subscribers
     else:
         return 0
